5b94aa0cf477a26279cf3bc177e8a327be800e62
anupam022 <anupamsharma237@gmail.com>
2022-03-26 10:50:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
+import './App.css';
+import NewComponent from './components/newComponent';
+// import SearchBar from './components/searchBar';
+import Button from '@mui/material/Button';
+
+toast.configure()
 function App() {
+  const  notify = () => {
+    toast('NASA HACKED',{position: toast.POSITION.TOP_LEFT})
+    toast.success('NASA HACKED',{position: toast.POSITION.TOP_CENTER})
+    toast.info('NASA HACKED',{position: toast.POSITION.TOP_RIGHT})
+    toast.warn('NASA HACKED',{position: toast.POSITION.BOTTOM_LEFT})
+    toast.erro('NASA HACKED',{position: toast.POSITION.BOTTOM_RIGHT})
+    toast('NASA HACKED',{position: toast.POSITION.BOTTOM_CENTER})
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <NewComponent></NewComponent>
+      {/* <SearchBar></SearchBar> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +34,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+    <Button 
+    onClick={notify}
+    type="submit" color="secondary" variant="contained">ClickMe</Button>
+
+
     </div>
   );
 }
---
+++ src/components/newComponent.jsx
@@ -0,0 +1,7 @@
+import React,{Component} from 'react'
+function NewComponent(){
+    return(<div><img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png" width="1000" height="600" alt="NASA"/></div>
+    )
+    
+}
+export default NewComponent 
